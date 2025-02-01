372acd03b4a55e6d598c60c0ec788dc44ebb1cc1
VivaLaNature <agubaevnariman@gmail.com>
2025-02-01 23:00:34 +0500

    Added the "game"

---
+++ Flappy_Bird/Assets/Bird_script.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.XR;
+using UnityEngine;
+
+public class Bird_script : MonoBehaviour
+{
+    public LogicScript logic;
+    public Rigidbody2D birdRigidbody;
+    public float flapStrength;
+    public bool birdIsAlive = true;
+    public float birdDeadZoneUp = 18;
+    public float birdDeadZoneDown = -17;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
+        {
+            birdRigidbody.velocity = Vector2.up * flapStrength;
+        }
+
+        if (transform.position.y > birdDeadZoneUp || transform.position.y < birdDeadZoneDown)
+        {
+            logic.GameOver();
+            birdIsAlive = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.GameOver();
+        birdIsAlive = false;
+    }
+
+}
